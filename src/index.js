65075bcb18070627a4756a50c80b71149421269d
rafrex <code@rafrex.com>
2016-07-22 19:40:27 -0700

    Invoke new touch state constructor

--- src/index.js
+++ src/index.js
@@ -18,16 +18,12 @@ function setPointerListener() {
 
 }
 
-function TouchState() {
-
-}
-
 function getListenerType() {
   const dIt = detectIt;
   const type = dIt.deviceType;
   if (type === 'mouseOnly') return [setMouseListener, undefined];
   if (type === 'touchOnly' && dIt.hasTouchEventsApi) return [setTouchListener, undefined];
-  if (type === 'hybrid' && dIt.hasTouchEventsApi) return [setHybridListener, new TouchState];
+  if (type === 'hybrid' && dIt.hasTouchEventsApi) return [setHybridListener, new TouchState()];
   if (dIt.hasTouch && dIt.hasPointerEventsApi) return [setPointerListener, undefined];
   return undefined;
 }
